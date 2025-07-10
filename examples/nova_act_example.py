28c34c050d9b970c241445eb8952312f380dc09c
Gautham Elango <gautham.gg@gmail.com>
2025-07-09 20:14:11 -0700

    Update nova_act_example.py

--- examples/nova_act_example.py
+++ examples/nova_act_example.py
@@ -3,11 +3,17 @@ import fleet as flt
 from nova_act import NovaAct, ActResult
 
 
-async def main() -> None:
+async def main():
     instance = await flt.env.make("hubspot")
+    cdp_url = await instance.browser().cdp_url()
+    print("Devtools URL:", await instance.browser().devtools_url())
 
     def run_nova() -> ActResult:
-        with NovaAct(starting_page=instance.urls.app) as nova:
+        with NovaAct(
+            starting_page=instance.urls.app,
+            cdp_endpoint_url=cdp_url,
+            preview={"playwright_actuation": True},
+        ) as nova:
             return nova.act("Create a deal")
 
     await asyncio.to_thread(run_nova)
