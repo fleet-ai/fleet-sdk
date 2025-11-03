76ae7b92341b9c0967d61a22ab0f281da632b467
Gautham Elango <gautham.gg@gmail.com>
2025-11-03 14:47:50 -0800

    version bump

--- fleet/_async/base.py
+++ fleet/_async/base.py
@@ -25,7 +25,7 @@ from .exceptions import (
 try:
     from .. import __version__
 except ImportError:
-    __version__ = "0.2.71"
+    __version__ = "0.2.72"
 
 logger = logging.getLogger(__name__)
 
