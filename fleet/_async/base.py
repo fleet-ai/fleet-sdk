974492788a9a45b45b27efb5cd6981b218445730
Gautham Elango <gautham.gg@gmail.com>
2025-12-08 23:58:01 -0800

    0.2.79

--- fleet/_async/base.py
+++ fleet/_async/base.py
@@ -26,7 +26,7 @@ from .exceptions import (
 try:
     from .. import __version__
 except ImportError:
-    __version__ = "0.2.74"
+    __version__ = "0.2.79"
 
 logger = logging.getLogger(__name__)
 
