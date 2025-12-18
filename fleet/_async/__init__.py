470d11021e424dd195597c678e071ff43c5df39f
Gautham Elango <gautham.gg@gmail.com>
2025-12-18 03:59:58 -0800

    0.2.85

--- fleet/_async/__init__.py
+++ fleet/_async/__init__.py
@@ -44,7 +44,7 @@ from ..types import VerifierFunction
 from .. import env
 from . import global_client as _async_global_client
 
-__version__ = "0.2.84"
+__version__ = "0.2.85"
 
 __all__ = [
     # Core classes
