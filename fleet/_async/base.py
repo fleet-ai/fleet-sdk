a272473d2e09a9e913c1c2a226685e2171117e31
kevin <6740161+c45@users.noreply.github.com>
2025-12-30 19:24:23 -0500

    v0.2.100

--- fleet/_async/base.py
+++ fleet/_async/base.py
@@ -26,7 +26,7 @@ from .exceptions import (
 try:
     from .. import __version__
 except ImportError:
-    __version__ = "0.2.99"
+    __version__ = "0.2.100"
 
 logger = logging.getLogger(__name__)
 
--- fleet/base.py
+++ fleet/base.py
@@ -27,7 +27,7 @@ from .exceptions import (
 try:
     from . import __version__
 except ImportError:
-    __version__ = "0.2.99"
+    __version__ = "0.2.100"
 
 logger = logging.getLogger(__name__)
 
