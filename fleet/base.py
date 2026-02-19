f385160bae5a9b356b19122edf7a932b0d550965
Gautham Elango <gautham.gg@gmail.com>
2026-02-18 20:28:05 -0800

    0.2.110

--- fleet/base.py
+++ fleet/base.py
@@ -27,7 +27,7 @@ from .exceptions import (
 try:
     from . import __version__
 except ImportError:
-    __version__ = "0.2.109"
+    __version__ = "0.2.110"
 
 logger = logging.getLogger(__name__)
 
